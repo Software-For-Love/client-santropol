42f266e8f3e719f07c337a15ed028df6fa6f71f1
9polymath <dolaniawo15@gmail.com>
2022-03-12 13:56:51 -0500

    login form updates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,65 @@
+import { useState } from "react";
+import logo from "./santropol.svg";
+import "./App.css";
 import "antd/dist/antd.css";
-import Routes from "./Routes";
+import Button from "./components/Button";
+import { Row, Col } from "antd";
+import {
+  AssignSlotOverlay,
+  DeliveryOverlay,
+  ClientDeliveryOverlay,
+} from "./components/Modal";
+import NormalLoginForm from "./components/Users/login";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
-  return <Routes />;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [deliveryModalVisible, setDeliveryModalVisible] = useState(false);
+  const [clientDeliveryModalVisible, setClientDeliveryModalVisible] =
+    useState(false);
+
+  return (
+
+    <div className='App'>
+      <header className='App-header'>
+        <NormalLoginForm/>
+        {/*<AssignSlotOverlay*/}
+        {/*  visible={modalVisible}*/}
+        {/*  setVisible={setModalVisible}*/}
+        {/*/>*/}
+        {/*<DeliveryOverlay*/}
+        {/*  visible={deliveryModalVisible}*/}
+        {/*  setVisible={setDeliveryModalVisible}*/}
+        {/*/>*/}
+        {/*<ClientDeliveryOverlay*/}
+        {/*  visible={clientDeliveryModalVisible}*/}
+        {/*  setVisible={setClientDeliveryModalVisible}*/}
+        {/*/>*/}
+        {/*<Row gutter={16}>*/}
+        {/*  <Col>*/}
+        {/*    <Button onClick={() => setModalVisible(true)}>Default</Button>*/}
+        {/*  </Col>*/}
+        {/*  <Col>*/}
+        {/*    <Button*/}
+        {/*      type='secondary'*/}
+        {/*      rounded*/}
+        {/*      onClick={() => setDeliveryModalVisible(true)}*/}
+        {/*    >*/}
+        {/*      Secondary*/}
+        {/*    </Button>*/}
+        {/*  </Col>*/}
+        {/*  <Col>*/}
+        {/*    <Button*/}
+        {/*      type='tertiary'*/}
+        {/*      onClick={() => setClientDeliveryModalVisible(true)}*/}
+        {/*    >*/}
+        {/*      Tertiary*/}
+        {/*    </Button>*/}
+        {/*  </Col>*/}
+        {/*</Row>*/}
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ front-end/src/components/Users/login.js
@@ -0,0 +1,74 @@
+import { Form, Input, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+import "../../App.css";
+import {Link} from "react-router-dom";
+import logo from "../../santropol.svg";
+import Button from "../Button";
+
+const NormalLoginForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className='form'>
+     <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Login
+        </p>
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        className="userbox"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+       <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox style={{float:'left'}}>Remember me</Checkbox>
+        </Form.Item>
+           <a style={{float:'right'}} className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button style={{width:'100%'}}>
+          Log in
+        </Button>  or  <a href="">register now!</a>
+      </Form.Item>
+    </Form>
+  </div>
+  );
+};
+
+export default NormalLoginForm;
