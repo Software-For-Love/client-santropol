5f644726b0c012fba2c6d9d11379184982f2dd17
tyrellshawn <ooluk098@uottawa.ca>
2022-06-12 15:05:34 -0400

    edit events working now and get event bug fixed

--- back-end/routes/eventRoutes.js
+++ back-end/routes/eventRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const eventRouter = express.Router();
-const { getFirestore, getDocs,setDoc, doc,collection, query, where } = require('firebase/firestore');
+const { getFirestore, getDocs,setDoc,updateDoc, doc,collection, query, where } = require('firebase/firestore');
 
 
 
@@ -14,7 +14,7 @@ const { getFirestore, getDocs,setDoc, doc,collection, query, where } = require('
  */
 eventRouter.get('/getEvents', async(req,res)=>{
     //Optional event type, default value is kitam
-    let eventType = req.body.eventType?req.query.eventType:"kitam";
+    let eventType = req.body.eventType?req.body.eventType:"kitam";
     const db = getFirestore();
     const today = req.body.eventDate? req.query.eventDate:new Date();
     var year =  today.getFullYear()%100;
@@ -73,13 +73,66 @@ eventRouter.post('/createEvent', async(req,res)=>{
     const userId = req.body.userId;
     const slot = req.body.slot;
     const date = req.body.eventDate? req.body.eventDate: Date.now();
-    const userComment = req.body.userComment;
+    const userComment = req.body.userComment?req.body.userComment:"";
     const db = getFirestore();
     var dbDate = parseInt(date);
 
 
     const userEventRef = doc(collection( db,"event"));
     await setDoc(userEventRef, {
+        uid: userId,
+        event_date: dbDate,
+        slot: slot,
+        event_type: eventType,
+        first_name: firstName,
+        last_name: lastName,
+        user_comment: userComment
+    }).catch(err => res.json({success: false, result: err}))
+    .then( writeResult => {
+        res.json({success: true, result: writeResult});
+    });
+
+
+    //get all the events for the current week   
+    
+});
+
+/**
+ *  Request to create an Event for a user:
+ * @description:
+ * For a volunteer we are creating an event if they havent worked 3 shifts this current week
+ * For a  staff we just always create the event. 
+ * When an event is created we need to check if the event time slot exists for that event. 
+ * If the event time slot does not exist we just create it. If it does then we add the existing
+ *  event time slot referemce to the user event document.
+ * 
+ * @param event_id the ID of the event document to be edited **Mandatory
+ * @param userId
+ * @param userType
+ * @param eventDate
+ * @param eventType
+ * @param firstName
+ * @param comment
+ * @param slot;
+ */
+ eventRouter.post('/editEvent', async(req,res)=>{
+    const eventId = req.body.event_id;
+    if(!eventId){
+        res.json({success: false, result: "Event ID is required"})
+    }
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const eventType =req.body.eventType;
+    const userId = req.body.userId;
+    const slot = req.body.slot;
+    const date = req.body.eventDate? req.body.eventDate: Date.now();
+    const userComment = req.body.userComment?req.body.userComment:"";
+    const db = getFirestore();
+    var dbDate = parseInt(date);
+
+
+    const userEventRef = doc(collection( db,"event"),eventId);
+    await updateDoc(userEventRef, {
         uid: userId,
         event_date: dbDate,
         slot: slot,
