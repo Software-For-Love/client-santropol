d9f0f45c997a36df42288436eaf72f6f054316a9
Dolapo Olaniawo <43099885+9polymath@users.noreply.github.com>
2022-06-12 11:36:12 -0400

    Switch backend to app engine (#42)
    * app engine deployment
    
    * app engine deployment

--- front-end/src/environment.js
+++ front-end/src/environment.js
@@ -2,3 +2,4 @@ export const devFlag = process.env.NODE_ENV === "development";
 export const backendServer = devFlag
   ? "http://localhost:2000/"
   : "https://santro.herokuapp.com/";
+  "https://unique-terminus-353014.nn.r.appspot.com"
