7a6d131ea670272c178a7be8c18882d857533a7d
Michael Kagnew <mike.kagnew@gmail.com>
2022-07-23 13:45:24 -0400

    removed unnecessary functionality, fixed some naming

--- back-end/routes/eventRoutes.js
+++ back-end/routes/eventRoutes.js
@@ -239,10 +239,9 @@ eventRouter.post("/editEvent", async (req, res) => {
   
   @param key: The key of the unique event in firestore that is having the current user removed
   @param role: Role of the user making the request -> volunteer, staff, or admin
-  @param uid:
+  @param uid
   @param reason: The reason for the cancellation
   Optional bodyParam:
-  @param  doc_name Optional if the key of an object isn't set
 
 */
 eventRouter.post("/removeUserFromEvent", async (req, res) => {
@@ -254,10 +253,6 @@ eventRouter.post("/removeUserFromEvent", async (req, res) => {
   if(req.body.key && req.body.role){
     q = query(eventCollectionRef,where("key", "==", req.body.key));
     event = (await getDocsWrapper(q))[0];
-  }
-  else if(req.body.doc_name && req.body.role) {
-    q = doc(db, "event", req.body.doc_name);
-    event = await getDocWrapper(q);
   }
   else {
     res.status(400).json({success: false, result: "No valid body parameters"})
@@ -311,7 +306,7 @@ eventRouter.get("/getUserPastEvents", async (req, res) => {
     } 
     else if (req.query.event_status == acceptedEventStates[0]) {
        q = query(
-        collection(db, "events"),
+        collection(db, "event"),
         where(req.query.event_status, "==", true),
         where("uid", "==", req.body.uid)
       );
@@ -320,7 +315,7 @@ eventRouter.get("/getUserPastEvents", async (req, res) => {
     } 
     else {
       q = query(
-        collection(db, "events"),
+        collection(db, "event"),
         where("uid", "==", req.body.uid)
       );
       let q2 = query(
@@ -352,15 +347,6 @@ function getDocsWrapper(query) {
     .catch((err) => {
       throw new Error(err);
     }); 
-}
-
-function getDocWrapper(docRef){
-  return getDoc(docRef).then((docSnapshot) => {
-    return docSnapshot;
-  }
-  ).catch(err => {
-    throw new Error(err);
-  })
 }
 
 
