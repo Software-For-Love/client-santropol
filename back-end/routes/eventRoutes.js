e95fb3b1f87453707d0c08e7feb10c20aa8219fa
Cem Gurel <gurel.cem374@hotmail.com>
2022-08-14 19:43:31 +0300

    changed next week day

--- back-end/routes/eventRoutes.js
+++ back-end/routes/eventRoutes.js
@@ -37,7 +37,7 @@ eventRouter.get("/getEvents", async (req, res) => {
   var nextweek = new Date(
     today.getFullYear(),
     today.getMonth(),
-    today.getDate() + 7
+    today.getDate() + 6
   );
   var nextweekYear = nextweek.getFullYear() % 100;
   var nextweekMonth =
