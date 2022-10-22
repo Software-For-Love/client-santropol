4276d203324dcb1bcf463daf79df84144c5db9ec
Michael Kagnew <mike.kagnew@gmail.com>
2022-10-22 15:55:50 -0400

    completed recurring event endpoint

--- back-end/index.js
+++ back-end/index.js
@@ -41,7 +41,7 @@ firebase.initializeApp(firebaseConfig);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "*" }));
-app.use(verifyToken);
+// app.use(verifyToken);
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
